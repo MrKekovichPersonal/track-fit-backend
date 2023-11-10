5f1b11203a1f9af4575ff5ba8b01732248f473b1
MrKekovich <mrkekovich.official@gmail.com>
2023-11-10 07:45:30 +0300

    feat: Added FoodItemEntity

---
+++ src/main/kotlin/com/mrkekovich/trackfitbackend/models/FoodItemEntity.kt
@@ -0,0 +1,35 @@
+package com.mrkekovich.trackfitbackend.models
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "food_items")
+class FoodItemEntity(
+    @Column(name = "name")
+    val name: String?,
+
+    @Column(name = "description")
+    val description: String?,
+
+    @Column(name = "image")
+    val image: String?,
+
+    @Column(name = "commentary")
+    val commentary: String?,
+
+    @Column(name = "calories")
+    val calories: Int?,
+
+    @Column(name = "protein")
+    val protein: Int?,
+
+    @Column(name = "carbs")
+    val carbs: Int?,
+
+    @Column(name = "fat")
+    val fat: Int?,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: String? = null
+)
