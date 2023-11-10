194af4f2a4513a6da2a518791183098a1d0074fc
MrKekovich <mrkekovich.official@gmail.com>
2023-11-10 07:19:44 +0300

    Initial commit

---
+++ src/main/kotlin/com/mrkekovich/trackfitbackend/TrackFitBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.mrkekovich.trackfitbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrackFitBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<TrackFitBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/mrkekovich/trackfitbackend/TrackFitBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mrkekovich.trackfitbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrackFitBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
